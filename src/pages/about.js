fd47a374e7e3198c59908fa81d193fcaee915c99
Macintosh Plus <macintoshplus@Macintoshs-iMac.local>
2021-10-26 13:43:12 +1100

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,17 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
-  return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
-  )
+    return (
+        <Layout pageTitle="Home Page">
+          <p>Mercer.</p>
+          <StaticImage
+            alt="mercer"
+            src="../images/mercer.jpg"
+          />
+        </Layout>
+      )
 }
 
 export default AboutPage
